1c77859e94e9ac63e511e8aefed81621f9f6ae32
Mc. Dharnel <mcdharnelpagaragan@gmail.com>
2023-09-19 00:15:24 +0800

    Adding projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -126,7 +126,7 @@ const Projects = () => {
             </div>
           </div>
           <div className="col-lg-8 col-md-8 col-12">
-            <p className='' style={{ color: '#E9DCC9' }}> <span className='about'>Welcome to The Music Player Project</span> Step into a world of rhythm and harmony with our Music Player Project. Crafted with APIs, it's your portal to endless melodies. Tune in, unwind, and let the music guide you. Welcome to a symphony of digital delight.</p>
+            <p className='' style={{ color: '#E9DCC9' }}> <span className='about'>Welcome to The Music Player Project 🎶</span> Step into a world of rhythm and harmony with our Music Player Project. Crafted with APIs, it's your portal to endless melodies. Tune in, unwind, and let the music guide you. Welcome to a symphony of digital delight.</p>
 
          
             <div><a href="https://mcdha.github.io/MusicPlayer/"><button type="button" className="btn btn-warning btnProjects"><i className="bi bi-eyeglasses pe-2"> </i>
@@ -150,6 +150,92 @@ const Projects = () => {
               {/* <i className="devicon-photoshop-plain colored"></i>
               <i className="devicon-xd-plain colored"></i>
               <i className="devicon-figma-plain colored"></i> */}
+            </div>
+            <hr className="horizontal-line" />
+
+    </section>
+
+
+    <section className='ps-3'>
+        <div data-aos="slide-left" data-aos-duration="500" className="row borderProjects">
+          <div className="col-lg-4 col-md-4 col-12 picturesProject">
+            <div className="workImageBox">
+              <img className='workImage' src="images/work/drumkit.png" alt="" />
+            </div>
+          </div>
+          <div className="col-lg-8 col-md-8 col-12">
+            <p className='' style={{ color: '#E9DCC9' }}> <span className='about'>Welcome to The Drum Kit Player Project 🥁</span> Step into a world of rhythm and beats with our Drum Kit Player. Simply press a key, and let the drums guide you. Explore various drum sounds, create your beats, and join a community of drum enthusiasts. Get ready to drum your way to digital delight.</p>
+
+         
+            <div><a href="https://drum-kit-gules-ten.vercel.app/"><button type="button" className="btn btn-warning btnProjects"><i className="bi bi-eyeglasses pe-2"> </i>
+                View
+            </button></a></div>
+           
+          </div>
+        </div>
+
+        <div className="dev-icons my-4" data-aos="slide-left" data-aos-duration="500">
+              <i className="devicon-html5-plain colored"></i>
+              <i className="devicon-css3-plain colored"></i>
+              <i className="devicon-javascript-plain colored"></i>
+              <i className="devicon-bootstrap-plain colored"></i>
+            </div>
+            <hr className="horizontal-line" />
+
+    </section>
+
+    <section className='ps-3'>
+        <div data-aos="slide-left" data-aos-duration="500" className="row borderProjects">
+          <div className="col-lg-4 col-md-4 col-12 picturesProject">
+            <div className="workImageBox">
+              <img className='workImage' src="images/work/simon.png" alt="" />
+            </div>
+          </div>
+          <div className="col-lg-8 col-md-8 col-12">
+            <p className='' style={{ color: '#E9DCC9' }}> <span className='about'>Welcome to The Simon Logic Game 🧩 </span> Enter the world of logical challenges with our Simon Game. Sharpen your memory and pattern recognition skills as you follow Simon's sequences of colors and sounds. Can you keep up with the ever-growing patterns and become the ultimate Simon champion? Join the game, test your wits, and let the fun begin.</p>
+
+         
+            <div><a href="https://simon-game-seven-livid.vercel.app/"><button type="button" className="btn btn-warning btnProjects"><i className="bi bi-eyeglasses pe-2"> </i>
+                View
+            </button></a></div>
+           
+          </div>
+        </div>
+
+        <div className="dev-icons my-4" data-aos="slide-left" data-aos-duration="500">
+              <i className="devicon-html5-plain colored"></i>
+              <i className="devicon-css3-plain colored"></i>
+              <i className="devicon-javascript-plain colored"></i>
+              <i className="devicon-bootstrap-plain colored"></i>
+            </div>
+            <hr className="horizontal-line" />
+
+    </section>
+
+
+    <section className='ps-3'>
+        <div data-aos="slide-left" data-aos-duration="500" className="row borderProjects">
+          <div className="col-lg-4 col-md-4 col-12 picturesProject">
+            <div className="workImageBox">
+              <img className='workImage' src="images/work/tictactoe.png" alt="" />
+            </div>
+          </div>
+          <div className="col-lg-8 col-md-8 col-12">
+            <p className='' style={{ color: '#E9DCC9' }}> <span className='about'>Welcome to The Ultimate Tic-Tac-Toe Showdown 🎮 </span> Immerse yourself in the classic game of Tic-Tac-Toe with a twist. Challenge your friends or our AI opponent to strategic battles on the grid. Will you conquer with Xs or Os? Claim your victory, and enjoy endless fun in the world of Tic-Tac-Toe.</p>
+
+         
+            <div><a href="https://tictactoe-game-seven.vercel.app/"><button type="button" className="btn btn-warning btnProjects"><i className="bi bi-eyeglasses pe-2"> </i>
+                View
+            </button></a></div>
+           
+          </div>
+        </div>
+
+        <div className="dev-icons my-4" data-aos="slide-left" data-aos-duration="500">
+              <i className="devicon-html5-plain colored"></i>
+              <i className="devicon-css3-plain colored"></i>
+              <i className="devicon-javascript-plain colored"></i>
+              <i className="devicon-bootstrap-plain colored"></i>
             </div>
             <hr className="horizontal-line" />
 
